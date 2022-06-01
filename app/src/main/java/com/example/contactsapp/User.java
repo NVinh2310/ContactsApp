256386d5f9e212017705d1b2a725b231105f9afd
NVinh2310 <vanvinhqn2310@gmail.com>
2022-06-01 09:38:26 +0700

    3th commit

--- app/src/main/java/com/example/contactsapp/User.java
+++ app/src/main/java/com/example/contactsapp/User.java
@@ -1,17 +1,17 @@
 package com.example.contactsapp;
 
 public class User {
-    String userName;
+    String id;
 
-    public User(String userName) {
-        this.userName = userName;
+    public User(String id) {
+        this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getId() {
+        return id;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setId(String id) {
+        this.id = id;
     }
 }
